287cc9cde65a5d4de5cc5d6c07f1af4774460506
Jeremy Goulet <jeremy.goulet14@gmail.com>
2021-12-26 19:45:52 -0500

    Update to all the components to work on Apple Silicon

--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,119 @@
 'use strict';
 
+const { src, dest, watch } = require('gulp');
+
 var gulp = require('gulp'),
+    sass = require('gulp-sass')(require('sass')),
     merge = require('merge-stream'),
-    sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify'),
     browserSync = require('browser-sync').create(),
     svgo = require('gulp-svgo'),
     gm = require('gulp-gm');
 
-// SASS task
-gulp.task('sass', function () {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'expanded'
-        }).on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('assets/css'))
-        .pipe(browserSync.stream());
-});
+// Compile SASS task
+function compileSass(done) {
+  src('src/scss/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass({
+      outputStyle: 'expanded'
+  }).on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(dest('assets/css'))
+  .pipe(browserSync.stream());
+  done();
+}
+exports.compileSass = compileSass
 
-// SASS task
-gulp.task('prod', function () {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(gulp.dest('assets/css'));
-});
+// Compile SASS task for Prod
+function prod(done) {
+  src('src/scss/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass({
+      outputStyle: 'compressed'
+  }).on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(dest('assets/css'));
+  done();
+}
+exports.prod = prod
 
 // Sass-Watch task
-gulp.task('watch', ['sass'], function() {
-    gulp.watch("src/scss/**/*.scss", ['sass']);
-    gulp.watch("assets/xml/**/*.xml").on('change', browserSync.reload);
-    gulp.watch("**/*.php").on('change', browserSync.reload);
+gulp.task('watch', function() {
+  watch('src/scss/**/*.scss', compileSass);
+  watch('assets/xml/**/*.xml').on('change', browserSync.reload);
+  watch('**/*.php').on('change', browserSync.reload);
 });
 
-// Img Task
-gulp.task('img', ['imgConvert', 'fullsize']);
-
-// Image convert
-gulp.task('imgConvert', function () {
+// Image convert 1x
+gulp.task('img1x', function () {
     // 1x
-    gulp.src('src/images/**/hero.png')
+    return src('src/images/**/hero.png')
         .pipe(gm(function (gmfile) {
             return gmfile.setFormat('png').quality(95);
         }, {
             imageMagick: true
         }))
-        .pipe(gulp.dest('assets/images'));
+        .pipe(dest('assets/images'));
+})
+
+// Image convert 2-3x
+gulp.task('img2x', function () {
     // 2x, 3x
-    gulp.src('src/images/**/hero{@2x,@3x}.png')
+    return src('src/images/**/hero{@2x,@3x}.png')
         .pipe(gm(function (gmfile) {
             return gmfile.setFormat('png').quality(75);
         }, {
             imageMagick: true
         }))
-        .pipe(gulp.dest('assets/images'));
+        .pipe(dest('assets/images'));
 })
 
 // Fullsize
-gulp.task('fullsize', ['imgConvert'], function () {
+gulp.task('fullsize', function() {
     return gulp.src('src/images/**/fullsize.png')
-        .pipe(gulp.dest('assets/images'));
-})
+      .pipe(gulp.dest('assets/images'));
+});
+
+gulp.task('imgConvert', gulp.series('img1x', 'img2x', async function() {
+    console.log('Images converted successfully. IMG CONVERT');
+}));
+
+gulp.task('img', gulp.series('fullsize', 'imgConvert', async function() {
+    console.log('Images converted successfully. IMG');
+}));
 
 // UglifyJS task
 gulp.task('uglify', function () {
-    return gulp.src('assets/js/*.js')
+    return src('assets/js/*.js')
         .pipe(uglify({
             compress: {
                 drop_console: true
             }
         }))
-        .pipe(gulp.dest('assets/js/'));
+        .pipe(dest('assets/js/'));
+});
+
+// jQuery task
+gulp.task('copy-jquery', function() {
+   return src('node_modules/jquery/dist/jquery.min.js')
+      .pipe(dest('assets/js/jquery/'));
 });
 
 // SVG task
 gulp.task('svg', function () {
-    return gulp.src('src/images/icons/**/*.svg')
+    return src('src/images/icons/**/*.svg')
         .pipe(svgo())
-        .pipe(gulp.dest('assets/images/icons/'));
+        .pipe(dest('assets/images/icons/'));
 });
 
-// Node Task
-gulp.task('npm', function () {
-    var jquery = gulp.src('node_modules/jquery/dist/jquery.min.js')
-        .pipe(gulp.dest('assets/js/jquery/'));
-
-    var requirejs = gulp.src('node_modules/requirejs/require.js')
-        .pipe(gulp.dest('assets/js/requirejs/'));
-
-    return merge(jquery, requirejs);
+// RequireJS task
+gulp.task('copy-requirejs', function() {
+   return src('node_modules/requirejs/require.js')
+      .pipe(dest('assets/js/requirejs/'));
 });
 
 // Default task
-gulp.task('default', ['watch', 'npm']);
+gulp.task('default', gulp.series('copy-requirejs', 'copy-jquery', async function() {
+    console.log('Build completed.');
+}));
