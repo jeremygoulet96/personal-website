679c34023c01a015b05dd6dc9f232b9db9720c87
Jeremy Goulet <jeremy.goulet14@gmail.com>
2022-01-05 20:27:22 -0500

    Reworked the whole Header and Nav on mobile. Added support for Firefox (backdrop-filter)

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,12 @@
 $(document).ready(function() {
-  // $('.header__action').on('click', openCloseNav(event));
+
   $('.header__action').on('click', function(event) {
     event.preventDefault();
-
-    let btn = $('.header__action');
-
-    $('.header').toggleClass('opened');
-
-    if(btn.text() == "Menu") {
-      btn.text("Fermer");
-      btn.removeClass("icon--hamburger");
-      btn.addClass("icon--close");
-    }
-    else {
-      btn.text("Menu");
-      btn.removeClass("icon--close");
-      btn.addClass("icon--hamburger");
-    }
+    openCloseNav();
   });
 
   $(window).scroll(function(){
-    if ($(this).scrollTop() > 0) {
+    if($(this).scrollTop() > 0) {
        $('.header').addClass('header--inTransition');
     } else {
        $('.header').removeClass('header--inTransition');
@@ -28,29 +14,42 @@ $(document).ready(function() {
   });
 });
 
-// function showHideHeader() {
-//   if ($(this).scrollTop() > 0) {
-//      $('.header').addClass('header--inTransition');
-//   } else {
-//      $('.header').removeClass('header--inTransition');
-//   }
-// }
+function openCloseNav() {
+  let btn = $('.header__action');
+  var navHeight = $('.nav').height();
+  var headerHeight = $('.header').height();
+  var translateNav = navHeight - headerHeight;
 
-// function openCloseNav(e) {
-//   e.preventDefault();
-//
-//   let btn = $('.header__action');
-//
-//   $('.header').toggleClass('opened');
-//
-//   if(btn.text() == "Menu") {
-//     btn.text("Fermer");
-//     btn.removeClass("icon--hamburger");
-//     btn.addClass("icon--close");
-//   }
-//   else {
-//     btn.text("Menu");
-//     btn.removeClass("icon--close");
-//     btn.addClass("icon--hamburger");
-//   }
-// }
+  $('.header').toggleClass('opened');
+
+  // OPEN
+  if(btn.text() == "Menu") {
+    btn.text("Fermer");
+    btn.removeClass("icon--hamburger");
+    btn.addClass("icon--close");
+    $('.nav').css('transform', 'translateY(' + translateNav+"px" + ')');
+    disableScroll();
+  } // CLOSE
+  else {
+    enableScroll()
+    btn.text("Menu");
+    btn.removeClass("icon--close");
+    btn.addClass("icon--hamburger");
+    $('.nav').css('transform', 'translateY(0px)');
+  }
+}
+
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+
+function enableScroll() {
+    window.onscroll = function() {};
+}
